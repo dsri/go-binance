7c68b394fbf6d63fa08a8e8a159aa711b2242b81
@dsri <dkbsri@gmail.com>
2018-12-08 13:52:22 -0500

    order info: add cumulative quote quantity

--- order_service.go
+++ order_service.go
@@ -250,19 +250,20 @@ func (s *GetOrderService) Do(ctx context.Context, opts ...RequestOption) (res *O
 
 // Order define order info
 type Order struct {
-	Symbol           string `json:"symbol"`
-	OrderID          int64  `json:"orderId"`
-	ClientOrderID    string `json:"clientOrderId"`
-	Price            string `json:"price"`
-	OrigQuantity     string `json:"origQty"`
-	ExecutedQuantity string `json:"executedQty"`
-	Status           string `json:"status"`
-	TimeInForce      string `json:"timeInForce"`
-	Type             string `json:"type"`
-	Side             string `json:"side"`
-	StopPrice        string `json:"stopPrice"`
-	IcebergQuantity  string `json:"icebergQty"`
-	Time             int64  `json:"time"`
+	Symbol              string `json:"symbol"`
+	OrderID             int64  `json:"orderId"`
+	ClientOrderID       string `json:"clientOrderId"`
+	Price               string `json:"price"`
+	OrigQuantity        string `json:"origQty"`
+	ExecutedQuantity    string `json:"executedQty"`
+	CumltvQuoteQuantity string `json:"cummulativeQuoteQty"`
+	Status              string `json:"status"`
+	TimeInForce         string `json:"timeInForce"`
+	Type                string `json:"type"`
+	Side                string `json:"side"`
+	StopPrice           string `json:"stopPrice"`
+	IcebergQuantity     string `json:"icebergQty"`
+	Time                int64  `json:"time"`
 }
 
 // ListOrdersService list all orders
